abaa27f7f4a1bdaaf694bdd03487b1062127a870
gwenpowers1 <99507192+gwenpowers1@users.noreply.github.com>
2023-08-10 16:31:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='ds5100_montecarlo',
     version='1.0.0',
+    author = 'Gwen Powers',
+    author_email = 'gp8cf@virginia.edu',
+    url = 'https://github.com/gwenpowers1/gwenpowers1_ds5100_montecarlo',
+    license = 'MIT',
     packages=find_packages(),
     install_requires=[
         'numpy',
